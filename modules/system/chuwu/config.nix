3ea2d784b45574a73bbfc301fa17719ab6140da7
quintuple-lained <quintuple_lained@proton.me>
2025-06-05 01:31:49 +0200

    rotation

--- modules/system/chuwu/config.nix
+++ modules/system/chuwu/config.nix
@@ -12,25 +12,64 @@
       "xfs"
     ];
     zfs.forceImportRoot = false;
-    kernelParams = ["fbcon=rotate:1"];
+    kernelParams = [
+      "fbcon=rotate:1"           # Console rotation (90° left)
+      "video=DSI-1:panel_orientation=left_side_up"  # Panel orientation
+    ];
   };
 
   networking = {
     hostName = "chuwu";
     hostId = "7f630874";
+    networkmanager.enable = true;  # Add this line
   };
+  
   services.openssh.enable = true;
 
   services.desktopManager.plasma6.enable = true;
 
+  # SDDM rotation configuration (chuwu-specific)
+  services.displayManager.sddm.settings = {
+    # X11 settings for rotation
+    X11 = {
+      DisplayCommand = ''
+        ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --rotate left || \
+        ${pkgs.xorg.xrandr}/bin/xrandr --output eDP1 --rotate left || \
+        ${pkgs.xorg.xrandr}/bin/xrandr --output LVDS-1 --rotate left || \
+        ${pkgs.xorg.xrandr}/bin/xrandr --output LVDS1 --rotate left || true
+      '';
+    };
+  };
+
   services.xserver = {
     enable = true;
     xkb.layout = "us";
+    # X11 display rotation
+    displayManager.setupCommands = ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --rotate left || true
+      ${pkgs.xorg.xrandr}/bin/xrandr --output eDP1 --rotate left || true
+      ${pkgs.xorg.xrandr}/bin/xrandr --output LVDS-1 --rotate left || true
+      ${pkgs.xorg.xrandr}/bin/xrandr --output LVDS1 --rotate left || true
+    '';
   };
+  
   hardware.steam-hardware.enable = true;
   hardware.bluetooth.enable = true;
 
   hardware.cpu.intel.updateMicrocode = true;
   powerManagement.enable = true;
   services.thermald.enable = true;
+
+  # Additional display rotation setup
+  environment.etc."sddm/scripts/Xsetup" = {
+    text = ''
+      #!/bin/sh
+      # Rotate display for SDDM
+      ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --rotate left 2>/dev/null || \
+      ${pkgs.xorg.xrandr}/bin/xrandr --output eDP1 --rotate left 2>/dev/null || \
+      ${pkgs.xorg.xrandr}/bin/xrandr --output LVDS-1 --rotate left 2>/dev/null || \
+      ${pkgs.xorg.xrandr}/bin/xrandr --output LVDS1 --rotate left 2>/dev/null || true
+    '';
+    mode = "0755";
+  };
 }
