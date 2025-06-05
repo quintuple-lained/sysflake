399e1dcd7e48cbd43c63e059156bd6dd2043d7c5
quintuple-lained <quintuple_lained@proton.me>
2025-06-05 02:48:47 +0200

    fixed vterm

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -127,19 +127,19 @@
     '';
 
     # Interactive shell setup
-    interactiveShellInit = ''
-      # Your vterm fish_prompt override
-      if functions -q fish_prompt
-        functions --copy fish_prompt vterm_old_fish_prompt
-        function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
-          # Remove the trailing newline from the original prompt. This is done
-          # using the string builtin from fish, but to make sure any escape codes
-          # are correctly interpreted, use %b for printf.
-          printf "%b" (string join "\n" (vterm_old_fish_prompt))
-          vterm_prompt_end
-        end
-      end
-    '';
+    # interactiveShellInit = ''
+    #   # Your vterm fish_prompt override
+    #   if functions -q fish_prompt
+    #     functions --copy fish_prompt vterm_old_fish_prompt
+    #     function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
+    #       # Remove the trailing newline from the original prompt. This is done
+    #       # using the string builtin from fish, but to make sure any escape codes
+    #       # are correctly interpreted, use %b for printf.
+    #       printf "%b" (string join "\n" (vterm_old_fish_prompt))
+    #       vterm_prompt_end
+    #     end
+    #   end
+    # '';
     
     # Fisher plugins converted to Home Manager
     plugins = [
