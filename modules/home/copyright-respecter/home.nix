05a5aeab6637fe2b36f8417a7e4122a693a6109d
quintuple-lained <quintuple_lained@proton.me>
2025-06-07 20:21:09 +0200

    more placeholder

---
+++ modules/home/copyright-respecter/home.nix
@@ -0,0 +1,34 @@
+{
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ../generic.nix
+    ../desktop
+    ../plasma
+  ];
+
+  programs.home-manager.enable = true;
+  home.stateVersion = "25.05";
+
+  home.username = "zoe";
+  home.homeDirectory = "/home/zoe";
+
+  home.packages = 
+    let
+      development = with pkgs; [
+      ];
+
+      fonts = with pkgs; [
+      ];
+
+      misc-packages = with pkgs; [
+        yt-dlp
+        networkmanager
+      ];
+
+      graphical = with pkgs; [];
+    in
+  development ++ fonts ++ misc-packages ++ graphical;
+}
