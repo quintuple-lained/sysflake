699ea4c127e227d47e86fae3d26fc784457a9597
quintuple-lained <quintuple_lained@proton.me>
2025-06-12 16:34:43 +0200

    fixes

--- modules/system/copyright-respecter/config.nix
+++ modules/system/copyright-respecter/config.nix
@@ -1,16 +1,21 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 
 {
   imports = [
     ./hardware-config.nix
-    ../../srv/ssh/ssh-server.nix
+    #../../srv/ssh/ssh-server.nix
     #../../srv/services/vpn-server.nix
     #../../srv/services/jellyfin.nix
     #../../srv/services/nextcloud.nix
     #../../srv/services/torrent.nix
     #../../srv/services/pihole.nix
   ];
-  
+
   networking = rec {
     hostName = "copyright-respecter";
     hostId = builtins.substring 0 8 (builtins.hashString "sha256" hostName);
