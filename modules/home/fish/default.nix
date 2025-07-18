a16dc90ccc4f8f687bcb919a689d48badba0eb54
quintuple-lained <160709820+quintuple-lained@users.noreply.github.com>
2025-07-18 11:12:34 +0000

    added fish autolaunch

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -208,4 +208,13 @@
   home.file = {
     # ".config/fish/functions/complex_function.fish".source = ./functions/complex_function.fish;
   };
+  programs.bash = {
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
 }
