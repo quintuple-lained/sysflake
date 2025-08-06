a2390dd36462639a10443ced94be3e3a8127582c
zoe <quintuple_lained@proton.me>
2025-08-06 11:05:10 +0200

    nextcloud the fith

--- modules/nix/services/nextcloud.nix
+++ modules/nix/services/nextcloud.nix
@@ -16,25 +16,7 @@
     };
   };
 
-  # Create necessary directories and CAN_INSTALL file
-  systemd.tmpfiles.rules = [
-    "d /main_pool/appdata/nextcloud 0750 nextcloud nextcloud"
-    "f /main_pool/appdata/nextcloud/CAN_INSTALL 0644 nextcloud nextcloud"
-  ];
-
   services = {
-    # PostgreSQL database
-    postgresql = {
-      enable = true;
-      package = pkgs.postgresql_15;
-      ensureDatabases = [ "nextcloud" ];
-      ensureUsers = [
-        {
-          name = "nextcloud";
-          ensureDBOwnership = true;
-        }
-      ];
-    };
 
     # Nextcloud
     nextcloud = {
@@ -45,25 +27,16 @@
       # Use ZFS pool for data
       home = "/main_pool/appdata/nextcloud";
 
-      # Database config
-      database.createLocally = true;
+      database = {
+        createLocally = true;
+      };
       config = {
         dbtype = "pgsql";
-        dbuser = "nextcloud";
-        dbhost = "/run/postgresql";
-        dbname = "nextcloud";
         adminuser = "admin";
         adminpassFile = config.sops.secrets.nextcloud_admin_password.path;
       };
-
-      # Basic settings
-      settings = {
-        default_phone_region = "DE";
-        trusted_domains = [
-          "192.168.178.109"
-          "localhost"
-        ];
-      };
+      configureRedis = true;
+      maxUploadSize = "16G";
     };
   };
 
