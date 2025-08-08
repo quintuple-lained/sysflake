3e5fd9a00234d1c37729a27e9b489183153bb7bc
quintuple-lained <quintuple_lained@proton.me>
2025-08-08 13:08:41 +0200

    removed markdown lint

--- flake.nix
+++ flake.nix
@@ -204,7 +204,6 @@
             check-yaml.enable = true;
             check-json.enable = true;
             check-toml.enable = true;
-            markdownlint.enable = true;
             check-merge-conflicts.enable = true;
             check-added-large-files.enable = true;
             end-of-file-fixer.enable = true;
