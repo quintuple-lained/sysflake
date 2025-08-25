2bde480046868714cc9c49d644d50ec9b2f365ab
quintuple-lained <quintuple_lained@proton.me>
2025-08-25 13:16:51 +0200

    better

--- modules/home/emacs/config/packages.el
+++ modules/home/emacs/config/packages.el
@@ -18,7 +18,37 @@
 
       (use-package vdiff)
 
-      (use-package projectile)
+      (use-package projectile
+  :init (projectile-mode +1)
+  :bind (:map projectile-mode-map
+         ("C-c p" . projectile-command-map))
+  :custom
+  (projectile-completion-system 'default)
+  (projectile-enable-caching t)
+  (projectile-indexing-method 'alien)
+  (projectile-sort-order 'recentf)
+  (projectile-use-git-grep t)
+  (projectile-switch-project-action #'projectile-dired)
+  (projectile-project-search-path '("~/src/" "~/projects/"))
+  :config
+  (add-to-list 'projectile-globally-ignored-directories "node_modules")
+  (add-to-list 'projectile-globally-ignored-directories ".git")
+  (add-to-list 'projectile-globally-ignored-directories "target")
+  (add-to-list 'projectile-globally-ignored-directories "__pycache__"))
+
+(use-package rg
+  :bind (("M-s g" . rg)
+         ("M-s d" . rg-dwim)
+         ("M-s k" . rg-kill-saved-searches)
+         ("M-s l" . rg-list-searches)
+         ("M-s p" . rg-project)
+         ("M-s r" . rg-literal)
+         ("M-s s" . rg-save-search)
+         ("M-s S" . rg-save-search-as-name)
+         ("M-s t" . rg-literal))
+  :config
+  (rg-enable-default-bindings))
+
 
       (use-package toml-mode)
 
@@ -36,6 +66,13 @@
         (lsp-ui-mode)
         )
 
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
 (use-package vterm
   :custom
   (setq-default explicit-shell-file-name "/bin/fish")
@@ -49,15 +86,66 @@
   (setq vterm-use-vterm-prompt-detection-method 'shell-integration)
 	)
 
-      (use-package smartparens
-        :bind
-        (
-         ("<localleader>(" . sp-wrap-round)
-         ("<localleader>{" . sp-wrap-curly)
-         ("<localleader>[" . sp-wrap-square)
-         ("<localleader>DEL" . sp-splice-sexp-killing-backward)
-         )
-        )
+(use-package smartparens
+  :hook ((prog-mode . smartparens-mode)
+         (text-mode . smartparens-mode))
+  :bind (:map smartparens-mode-map
+         ("C-M-f" . sp-forward-sexp)
+         ("C-M-b" . sp-backward-sexp)
+         ("C-M-d" . sp-down-sexp)
+         ("C-M-a" . sp-backward-down-sexp)
+         ("C-M-e" . sp-up-sexp)
+         ("C-M-u" . sp-backward-up-sexp)
+         ("C-M-t" . sp-transpose-sexp)
+         ("C-M-n" . sp-next-sexp)
+         ("C-M-p" . sp-previous-sexp)
+         ("C-M-k" . sp-kill-sexp)
+         ("C-M-w" . sp-copy-sexp)
+         ("M-<delete>" . sp-unwrap-sexp)
+         ("M-<backspace>" . sp-backward-unwrap-sexp)
+         ("C-<right>" . sp-forward-slurp-sexp)
+         ("C-<left>" . sp-forward-barf-sexp)
+         ("C-M-<left>" . sp-backward-slurp-sexp)
+         ("C-M-<right>" . sp-backward-barf-sexp)
+         ("M-D" . sp-splice-sexp)
+         ("C-M-<delete>" . sp-splice-sexp-killing-forward)
+         ("C-M-<backspace>" . sp-splice-sexp-killing-backward)
+         ("C-S-<backspace>" . sp-splice-sexp-killing-around)
+         ("C-]" . sp-select-next-thing-exchange)
+         ("C-<left_bracket>" . sp-select-previous-thing)
+         ("C-M-]" . sp-select-next-thing)
+         ("M-F" . sp-forward-symbol)
+         ("M-B" . sp-backward-symbol))
+  :config
+  (require 'smartparens-config)
+  (sp-use-paredit-bindings)
+  (show-paren-mode t))
+
+(use-package multiple-cursors
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c C-<" . mc/mark-all-like-this)
+         ("C-S-<mouse-1>" . mc/add-cursor-on-click)))
+
+(use-package markdown-mode
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :custom
+  (markdown-command "multimarkdown")
+  (markdown-live-preview-delete-export 'delete-on-export)
+  (markdown-asymmetric-header t)
+  (markdown-header-scaling t))
+
+(use-package dockerfile-mode
+  :mode "Dockerfile\\'")
+
+(use-package color-rg
+  :bind (("M-s M-s" . color-rg-search-input)
+         ("M-s M-p" . color-rg-search-project)
+         ("M-s M-f" . color-rg-search-input-in-current-file)))
+
 
       (use-package ace-jump-mode
         :bind
@@ -98,6 +186,25 @@
         (display-graphic-p)
         )
 
+      (use-package apheleia
+  :init (apheleia-global-mode +1)
+  :custom
+  (apheleia-remote-algorithm 'cancel)
+  :config
+  ;; Add formatters as needed
+  (setf (alist-get 'python-mode apheleia-mode-alist) 'black)
+  (setf (alist-get 'python-ts-mode apheleia-mode-alist) 'black)
+  (setf (alist-get 'rust-mode apheleia-mode-alist) 'rustfmt)
+  (setf (alist-get 'rust-ts-mode apheleia-mode-alist) 'rustfmt)
+  (setf (alist-get 'go-mode apheleia-mode-alist) 'gofmt)
+  (setf (alist-get 'go-ts-mode apheleia-mode-alist) 'gofmt)
+  (setf (alist-get 'js-mode apheleia-mode-alist) 'prettier-javascript)
+  (setf (alist-get 'js-ts-mode apheleia-mode-alist) 'prettier-javascript)
+  (setf (alist-get 'typescript-mode apheleia-mode-alist) 'prettier-typescript)
+  (setf (alist-get 'typescript-ts-mode apheleia-mode-alist) 'prettier-typescript)
+  (setf (alist-get 'nix-mode apheleia-mode-alist) 'nixfmt))
+
+
       (use-package dashboard
         :config
 
@@ -112,6 +219,23 @@
                               "i made a new emacs tutorial video, its around one and a half... years long, i can send it to you via ftp"
                               "but emacs is not a text editor"
                               "DNA sequencing? emacs!"
+ouse-package apheleia
+  :init (apheleia-global-mode +1)
+  :custom
+  (apheleia-remote-algorithm 'cancel)
+  :config
+  ;; Add formatters as needed
+  (setf (alist-get 'python-mode apheleia-mode-alist) 'black)
+  (setf (alist-get 'python-ts-mode apheleia-mode-alist) 'black)
+  (setf (alist-get 'rust-mode apheleia-mode-alist) 'rustfmt)
+  (setf (alist-get 'rust-ts-mode apheleia-mode-alist) 'rustfmt)
+  (setf (alist-get 'go-mode apheleia-mode-alist) 'gofmt)
+  (setf (alist-get 'go-ts-mode apheleia-mode-alist) 'gofmt)
+  (setf (alist-get 'js-mode apheleia-mode-alist) 'prettier-javascript)
+  (setf (alist-get 'js-ts-mode apheleia-mode-alist) 'prettier-javascript)
+  (setf (alist-get 'typescript-mode apheleia-mode-alist) 'prettier-typescript)
+  (setf (alist-get 'typescript-ts-mode apheleia-mode-alist) 'prettier-typescript)
+  (setf (alist-get 'nix-mode apheleia-mode-alist) 'nixfmt))
                               "with jupiter you're modifying the states of the program, with emacs youre modifying the state of the art"
                               "saying emacs will die is like saying turing complete will die"
                               "ive also been diagnosed with severe hostility to vim users, but thats not a real disease of course, the real disease is vim"
